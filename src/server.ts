7f12d46e37afdf848c161e011062d13d52855c74
Izharkhan2022 <izhar.khan@softqubes.com>
2025-10-23 11:03:25 +0530

    update imports and minor changes

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,12 @@ import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import path from "path";
 import appRoutes from "./routes/index";
-
-// Load environment variables
-dotenv.config();
+import { config } from "./config";
 
 // Configuration
-const PORT = process.env.PORT || 5000;
-const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+const PORT = config.port || 5000;
+const FRONTEND_URL = config.frontendUrl || "http://localhost:5173";
+
 
 // Initialize Express app
 const app = express();
